af0730d5035016210388c0d8758cdea6130e05ac
jason <jason.desen.guo@gmail.com>
2016-07-21 14:37:12 -0400

    objective c

---
+++ TapeEquilibrium/main.m
@@ -0,0 +1,41 @@
+//
+//  main.m
+//  objc-practice
+//
+//  Created by DesenGuo on 2016-07-09.
+//  Copyright © 2016 ideaweddinghome. All rights reserved.
+//
+
+int solution(int* A,size_t count) {
+    // SET temp to 2xarray[0]-sum
+    // SET min to |temp|
+    // FOR p = 1 to array size
+    // 	temp = temp + array[p]x2
+    // 	min=min(min, |temp|)
+    int sum = 0;
+    for (int i=0;i<count;i++)
+    {
+      sum=sum+A[i];
+    }
+    
+    int temp=A[0]*2-sum;
+    int min=abs(temp);
+    for(int p=2;p<count-1;p++)
+    {
+        temp=temp+A[p-1]*2;
+        min=abs(temp);
+    }
+    return min;
+}
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        int a[] = {3,1,2,4,3};
+        int result=solution(a,sizeof a / sizeof (int));
+        NSLog(@"%d",result);
+        
+    }
+    return 0;
+}
