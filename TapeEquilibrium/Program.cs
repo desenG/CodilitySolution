43c02e73c00bfacf2d5694d2f116fa202d61aaf9
jason <jason.desen.guo@gmail.com>
2016-07-21 14:51:23 -0400

    Update Program.cs

--- TapeEquilibrium/Program.cs
+++ TapeEquilibrium/Program.cs
@@ -16,7 +16,6 @@ namespace PracticeCSharp
 	{
 		public int solution(int[] A)
 		{
-			// write your code in Java SE 8
 			// SET temp to 2xarray[0]-sum
 			// SET min= temp
 			// FOR p = 1 to array size
